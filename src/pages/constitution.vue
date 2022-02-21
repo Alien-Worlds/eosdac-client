10f20b88074cf847f286a621e747e48d7b3c3fc4
Angelo Laub <angelolaub@gmail.com>
2022-02-21 16:28:18 +0100

    load membership terms (constitution) from eosdac-api instead of the wild wild web to prevent problems with missing CORS headers

--- src/pages/constitution.vue
+++ src/pages/constitution.vue
@@ -136,37 +136,23 @@ export default {
 
   methods: {
     async getConstitution () {
-      if (!this.getLatestMemberTerm.terms) return
+      if (!this.getLatestMemberTerm.terms) {
+        console.log('!this.getLatestMemberTerm.terms')
+        return
+      }
       this.isloading = true
-      try {
-        let getCt = await this.loadConstitutionFromGithub(
-          this.getLatestMemberTerm.terms
-        )
-        this.md5_constitution = CryptoJS.MD5(getCt).toString()
+      let getCt = await this.$store.dispatch('dac/fetchMemberTerms')
+      this.md5_constitution = CryptoJS.MD5(getCt).toString()
+      // check if the fetched constitution matches the contract hash
+      // if(this.md5_constitution === this.latestMemberTerms.hash){
+      //   console.log('Constitution verified! Hashes match!')
+      // }
 
-        // check if the fetched constitution matches the contract hash
-        // if(this.md5_constitution === this.latestMemberTerms.hash){
-        //   console.log('Constitution verified! Hashes match!')
-        // }
-
-        this.constitution =
-          '<span class="animate-fade">' +
-          marked(getCt, { sanitize: true }) +
-          '</span>'
-        this.isloading = false
-      } catch (e) {
-        console.log(e)
-        this.isloading = false
-      }
-    },
-
-    async loadConstitutionFromGithub (url) {
-      try {
-        let constitution = await this.$axios.get(url)
-        return constitution.data
-      } catch (err) {
-        throw err
-      }
+      this.constitution =
+        '<span class="animate-fade">' +
+        marked(getCt, { sanitize: true }) +
+        '</span>'
+      this.isloading = false
     },
 
     async signConstitution () {
